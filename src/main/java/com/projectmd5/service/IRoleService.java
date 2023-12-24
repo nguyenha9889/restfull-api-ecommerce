9193fd020adcd9e9519cd7602fefc782d3f09d6b
nguyenha9889 <nguyenha9889@gmail.com>
2023-12-24 21:48:29 +0700

    update product controller rest api

--- src/main/java/com/projectmd5/service/IRoleService.java
+++ src/main/java/com/projectmd5/service/IRoleService.java
@@ -3,6 +3,6 @@ package com.projectmd5.service;
 import com.projectmd5.model.entity.Role;
 import com.projectmd5.model.entity.RoleName;
 
-public interface IRoleService extends IGenericService<Role,RoleName, Long>{
+public interface IRoleService extends IGenericService<Role, Long>{
    Role findByRoleName(RoleName roleName);
 }
