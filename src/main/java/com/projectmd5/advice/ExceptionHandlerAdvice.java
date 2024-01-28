30830e13bb01feb6d51601708066a4d35b4d1b95
nguyenha9889 <nguyenha9889@gmail.com>
2024-01-28 22:38:23 +0700

    finish all api except wishlist

--- src/main/java/com/projectmd5/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/projectmd5/advice/ExceptionHandlerAdvice.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
 
